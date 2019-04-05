02f83ee0693876a7645745a93b87177225121f09
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-04 21:05:19 -0600

    formatting

--- i2cdevice.h
+++ i2cdevice.h
@@ -10,10 +10,9 @@ class I2CDevice {
     public:
         I2CDevice(uint8_t addr, const char *bus = "/dev/i2c-1");
         ~I2CDevice();
-
         ssize_t write(const uint8_t *buf, size_t count);
         ssize_t read(uint8_t *buf, size_t count);
+
     private:
         int _fd;
-        // uint8_t _addr;
 };
