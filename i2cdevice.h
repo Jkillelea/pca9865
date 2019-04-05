9316c2e75d45bbf59e94d9bae62e735a45d119f2
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-04 21:06:27 -0600

    formatting

--- i2cdevice.h
+++ i2cdevice.h
@@ -7,12 +7,12 @@
 #include <fcntl.h>
 
 class I2CDevice {
-    public:
-        I2CDevice(uint8_t addr, const char *bus = "/dev/i2c-1");
-        ~I2CDevice();
-        ssize_t write(const uint8_t *buf, size_t count);
-        ssize_t read(uint8_t *buf, size_t count);
+public:
+    I2CDevice(uint8_t addr, const char *bus = "/dev/i2c-1");
+    ~I2CDevice();
+    ssize_t write(const uint8_t *buf, size_t count);
+    ssize_t read(uint8_t *buf, size_t count);
 
-    private:
-        int _fd;
+private:
+    int _fd;
 };
