c5e759a098c00278b289a4d7e4814fc010424b5a
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-11 13:29:31 -0600

    formatting

--- pca9865.h
+++ pca9865.h
@@ -4,20 +4,20 @@
 /*
  * Copyright 2019 Jacob Killelea <jkillelea@protonmail.ch>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"), to deal in the 
- * Software without restriction, including without limitation the rights to use, copy, 
- * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
- * and to permit persons to whom the Software is furnished to do so, subject to the 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
  * following conditions:
- * The above copyright notice and this permission notice shall be included in all 
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
@@ -52,7 +52,7 @@ class PCA9865 {
         void enableRestart( bool immediate = true);
         void disableRestart(bool immediate = true);
         // we want the external clock disabled
-        void enableExtclk( bool immediate = true); 
+        void enableExtclk( bool immediate = true);
         void disableExtclk(bool immediate = true);
         // auto increment is a convience feature
         void enableAi( bool immediate = true);
@@ -83,7 +83,7 @@ class PCA9865 {
 
         // check if each of the config bits is set
         // NOTE: default behavior is to fetch the actual
-        // register off the chip each time. This does 
+        // register off the chip each time. This does
         // present the risk of a bad I2C packet overwriting
         // the variable with garbage data.
         // If that happens we can just request the data again.
